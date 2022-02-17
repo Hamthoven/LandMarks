03c8f87946aad9872948fe3cc03fc75ac3e05fe4
Hamthoven <hamthoven@gmail.com>
2022-02-17 16:13:51 +0900

    draw badge

---
+++ LandMarks/Views/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotaredBadgeSymbol.swift
+//  LandMarks
+//
+//  Created by 강유진 on 2022/02/17.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
